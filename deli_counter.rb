3a1bbcdde0c90920d8632647c96c420e3b353cea
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-31 08:01:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,7 +27,7 @@ end
 def now_serving katz_deli
   while katz_deli != []
     puts "Currently serving #{katz_deli[0]}." 
-    
+    katz_deli.shift
   end 
   puts "There is nobody waiting to be served!"
   
