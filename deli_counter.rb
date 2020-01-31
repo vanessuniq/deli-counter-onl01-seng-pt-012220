7ec4ab989e4ee1a0f7d1f8c70ca9a5a32b8aae4b
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-31 05:50:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ def line array
     if array == []
       puts "The line is currently empty"
     else
+      
       array.each_with_index do |value, index|
-      puts "The line is currently #{index + 1}. #{value}"
+       #{index + 1}. #{value}"
       end 
     end 
 end 
