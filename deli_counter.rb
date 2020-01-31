945545c992359f391f997a8897778124374dad88
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-31 05:40:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,8 @@ def take_a_number (katz_deli, name)
     katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
   else
-    
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
   
 end
 
