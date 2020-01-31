501a2fecf70c8c461efb79f00baf6c67cf286438
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-31 04:49:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,20 @@
 # Write your code here.
 katz_deli = []
-def line array 
-  array.each_with_index do |value, index|
-    until array.empty? 
+def line katz_deli
+  puts "The line is currently empty." if katz_deli.empty?
+end 
+
+def take_a_number (katz_deli, name)
+  katz_deli << name 
+  katz_deli.each_with_index do |name, value|
+    puts name + " " + (index += 1).to_s
+  end 
+end
+
+def now_serving katz_deli
+  until katz_deli.empty?
+    puts katz_deli.first 
+    katz_deli.shift
+  end 
   
-  if 
 end 
