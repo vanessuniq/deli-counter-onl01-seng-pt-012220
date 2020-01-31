60342f3882b850f476d518ac9c11f478463f58cc
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-31 06:42:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line array
       puts "#{index +1}. #{value}"
     end 
   end 
-  array.empty? puts "The line is currently empty." : puts 
+  array.empty? puts "The line is currently empty." : puts "The line is currently: #{}"
 end 
 
 def take_a_number (katz_deli, name)
