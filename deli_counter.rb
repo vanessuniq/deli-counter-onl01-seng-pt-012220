f8e5f31df3f0e0e221023e38d15e0060d9ab33de
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-31 05:12:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line katz_deli
     if katz_deli == []
       puts "The line is currently empty"
     else
-      puts "The line is currently #{index += 1}. #{value}"
+      puts "The line is currently #{index + 1}. #{value}"
     end 
   end 
 end 
